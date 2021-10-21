02a418045303f05adcfd7b7a96b57cdf072ad3b4
Kent <kentallananderson@gmail.com>
2021-10-21 17:58:48 -0500

    Late commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,17 @@ class ProductsController < ApplicationController
     products = Product.all
     render json: products.as_json
   end
+
+  def update
+    the_id = params[:id]
+    product = Product.find_by(id: the_id)
+    product.name = params[:name]
+    product.price = params[:price]
+    product.img_url = params[:img_url]
+    product.description params[:description]
+  
+    product.save
+
+    render json: product.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/first_product", controller: "products", action: "first"
   get "/all_products" => "products#all"
+  patch "/products/:id" => "products#update"
 end
